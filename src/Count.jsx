8183f95bfa72a6f801e9f87213c3c7b62bb6192e
Ridham <ridhammakvana99@gmail.com>
2025-04-23 17:13:55 +0530

    23-april

--- src/Count.jsx
+++ src/Count.jsx
@@ -1,19 +1,19 @@
-import React, { useState } from "react";
+import React, { useState } from "react"
 
 const Count = () => {
 
-    var [count, setCount] = useState(0);
+    var [count, setCount] = useState(0)
 
     const handleClick = () => {
-        setCount(count + 1);
-    };
+        setCount(count + 1)
+    }
 
     return (
         <div>                                     
             <h2>Count:{count}</h2>
             <button onClick={handleClick}>+</button>
         </div>
-    );
+    )
 }
 
 export default Count;
