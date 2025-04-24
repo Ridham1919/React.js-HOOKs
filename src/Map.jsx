1488546dba2c6057bf075c36bc0f54d7ac61b2ac
Ridham <ridhammakvana99@gmail.com>
2025-04-24 16:13:22 +0530

    24-april

--- src/Map.jsx
+++ src/Map.jsx
@@ -1,12 +1,11 @@
 import React from "react";
 
 const Map = () => {
-    
+
     var data = [{ "name": "ridham", "age": "17" }, { "name": "admin", "age": "18" }, { "name": "admin", "age": "18" },{ "name": "admin", "age": "18" },{ "name": "admin", "age": "18" },{ "name": "admin", "age": "17" },{ "name": "admin", "age": "17" }]
 
 
     return (
-
         <>
             <table border={1} cellPadding={7} cellSpacing={0}>
                 <tr>
